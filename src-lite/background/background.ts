33140afde675bfeba818e548203b3aec795e0f41
Zoe Kahala <452060+ZoeS17@users.noreply.github.com>
2021-08-08 07:38:42 -0500

    [Feature] Error handling added
    *Feature
    Error handling added to `background.ts` and `content-script.ts` to avoid throwing unhandled errors that may force the extension to unload.
    
    *Update
    Brought packages up to date with current dependabot PRs: #4 #5 #7 #8 and #9 . Updated #6 to version `y18n@4.0.3`

--- src-lite/background/background.ts
+++ src-lite/background/background.ts
@@ -1,45 +1,49 @@
 import { Message } from "../../src/shared/enums/message";
 import { MessagePassingService } from "../../src/shared/services/message-passing-service";
 
-chrome.contextMenus.create({
-    title: `Word counter - lite`,
-    contexts: ["selection"],
-    id: "WordsCounter",
-});
+try{
+    chrome.contextMenus.create({
+        title: `Word counter - lite`,
+        contexts: ["selection"],
+        id: "WordsCounter",
+    });
 
-MessagePassingService.addMessageListener(
-    { source: Message.ContentId, destination: Message.BackgroundId, name: "selectedText" },
-    (selectedText) => {
-        chrome.contextMenus.update("WordsCounter", {
-            title: `${formattedTextLong(selectedText)}`,
-        });
+    MessagePassingService.addMessageListener(
+        { source: Message.ContentId, destination: Message.BackgroundId, name: "selectedText" },
+        (selectedText) => {
+            chrome.contextMenus.update("WordsCounter", {
+                title: `${formattedTextLong(selectedText)}`,
+            });
+        }
+    );
+
+    function numberOfWords(text = "") {
+        return text.trim().split(" ").length;
     }
-);
 
-function numberOfWords(text = "") {
-    return text.trim().split(" ").length;
-}
-
-function numberOfCharacters(text = "") {
-    return text.trim().length;
-}
-
-function formattedTextLong(text = "") {
-    return `${numberOfWordsLocaleString(text)} - ${numberOfCharactersLocaleString(text)}`;
-}
-
-function numberOfWordsLocaleString(text = "") {
-    const nrOfWords = numberOfWords(text);
-    if (nrOfWords === 1) {
-        return `${nrOfWords} ${chrome.i18n.getMessage("_word")}`;
+    function numberOfCharacters(text = "") {
+        return text.trim().length;
     }
-    return `${nrOfWords} ${chrome.i18n.getMessage("_words")}`;
-}
 
-function numberOfCharactersLocaleString(text) {
-    const nrOfCharacters = numberOfCharacters(text);
-    if (nrOfCharacters === 1) {
-        return `${nrOfCharacters} ${chrome.i18n.getMessage("_character")}`;
+    function formattedTextLong(text = "") {
+        return `${numberOfWordsLocaleString(text)} - ${numberOfCharactersLocaleString(text)}`;
     }
-    return `${nrOfCharacters} ${chrome.i18n.getMessage("_characters")}`;
+
+    function numberOfWordsLocaleString(text = "") {
+        const nrOfWords = numberOfWords(text);
+        if (nrOfWords === 1) {
+            return `${nrOfWords} ${chrome.i18n.getMessage("_word")}`;
+        }
+        return `${nrOfWords} ${chrome.i18n.getMessage("_words")}`;
+    }
+
+    function numberOfCharactersLocaleString(text) {
+        const nrOfCharacters = numberOfCharacters(text);
+        if (nrOfCharacters === 1) {
+            return `${nrOfCharacters} ${chrome.i18n.getMessage("_character")}`;
+        }
+        return `${nrOfCharacters} ${chrome.i18n.getMessage("_characters")}`;
+    }
+} catch(err){
+    console.error(err);
 }
